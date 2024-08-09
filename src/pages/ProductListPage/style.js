e5f7fd06baed369063ec5796288d8d0c751f2d9c
jhonatalopes <jhonata.a.lopes88@gmail.com>
2024-08-09 00:29:07 -0300

    Create style pages

---
+++ src/pages/ProductListPage/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"; // sempre importar esse babado aqui, first line
+
+// modelo estrutural para estilização:
+export const Title = styled.p `
+    font-size: 24px;
+    color: red;
+
+`
+//importar no index.
+
+
---
+++ src/pages/ProductViewPage/index.jsx
@@ -0,0 +1,7 @@
+function ProductViewPage() {
+    return (
+        <p>Teste Product View</p>
+    )
+}
+
+export default ProductViewPage;
